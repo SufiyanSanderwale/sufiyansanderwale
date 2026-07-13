f5ca9d70ee88565f83f93f8836635832f1bfff8d
= <sufiyansanderwale54@gmail.com>
2026-07-13 15:39:16 +0530

    update sound.ts

--- src/lib/sound.ts
+++ src/lib/sound.ts
@@ -28,7 +28,7 @@ export function playClickSound() {
     if (!clickAudio) {
       clickAudio = new Audio(buttonSound);
       const isMobile = window.matchMedia("(max-width: 768px)").matches;
-      clickAudio.volume = isMobile ? 0.75 : 0.40; // pleasant, balanced volume level
+      clickAudio.volume = isMobile ? 0.90 : 0.37; // pleasant, balanced volume level
     } else {
       // Reset playback position if it's already playing or finished
       clickAudio.currentTime = 0;
@@ -53,7 +53,7 @@ export function playNavClickSound() {
 
     if (!navAudio) {
       navAudio = new Audio(navSound);
-      navAudio.volume = 0.35; // pleasant, balanced volume level
+      navAudio.volume = 0.23; // pleasant, balanced volume level
     } else {
       // Reset playback position if it's already playing or finished
       navAudio.currentTime = 0;
